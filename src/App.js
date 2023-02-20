37e8db0769d79d4907764d6cbe26086dac94c0a2
toto <riedotoine@gmail.com>
2023-02-20 17:32:20 +0100

    Add crypto-js, md5, autoprefixer, postcss, and tailwindcss dependencies, update App.js and App.css, and add Postcss and Hasher components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import { MD5 } from "crypto-js";
+import "./App.css";
+import Hasher from "./components/Hasher";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Content">
+      <Hasher />
     </div>
   );
 }
---
+++ src/components/Hasher.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { MD5 } from "crypto-js";
+
+function Hasher() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleHash = () => {
+    const hashedValue = MD5(input).toString();
+    setOutput(hashedValue);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(output);
+  };
+
+  return (
+    <div className="max-w-lg mx-auto mt-8 bg-white rounded-lg shadow-lg p-6">
+      <h2 className="text-2xl font-bold mb-4">MD5 Hash Generator</h2>
+      <div className="mb-4">
+        <label htmlFor="input" className="block font-medium mb-2">
+          Enter text to hash:
+        </label>
+        <input
+          type="password"
+          id="input"
+          value={input}
+          onChange={handleChange}
+          className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:border-indigo-500"
+        />
+      </div>
+      <button
+        onClick={handleHash}
+        className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-700"
+      >
+        Hash
+      </button>
+      {output && (
+        <div className="mt-4">
+          <label htmlFor="output" className="block font-medium mb-2">
+            Hashed value:
+          </label>
+          <div className="flex items-center">
+            <input
+              type="password"
+              id="output"
+              value={output}
+              readOnly
+              className="w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:border-indigo-500"
+            />
+            <button
+              onClick={handleCopy}
+              className="ml-2 bg-gray-200 text-gray-800 py-2 px-4 rounded-lg hover:bg-gray-300"
+            >
+              Copy
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Hasher;
